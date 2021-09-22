9bc37437dfd333f652d0e6d1b6bdbc2e6882f06d
hodokim <hodo323@naver.com>
2021-09-22 23:41:19 +0900

    test

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -20,7 +20,7 @@ import TodoFooter from './components/TodoFooter.vue'
 export default {
   components:{
     'TodoHeader':TodoHeader,
-    'TodoInput':TodoInput,
+    'TodoInput':TodoInput, 
     'TodoList':TodoList,
     'TodoFooter':TodoFooter,
   }
