16377e5d99001c1e7e2f418b652bfff11b9d31cf
hodokim <hodo323@naver.com>
2021-09-28 23:32:57 +0900

    Vue Study Day 5

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="inputBox shadow">
-      <input type="text" v-model="newTodoItem">
+      <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
       <!-- <button>add</button> -->
       <span class="addContainer" v-on:click="addTodo">
         <i class="fas fa-plus addBtn"></i>
+        <i class="fas fa-plus"></i>
       </span>
   </div>
 </template>
@@ -24,6 +25,7 @@ export default {
     clearInput: function() {
       this.newTodoItem='';
     }
+
   }
 }
 </script>
