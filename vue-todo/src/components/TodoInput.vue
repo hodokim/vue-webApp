8b755947464d5d1239a15e731b0a6fab2636c2c5
hodokim <hodo323@naver.com>
2021-10-06 23:39:29 +0900

    Vue Study Day 8

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -5,14 +5,25 @@
         <i class="fas fa-plus addBtn"></i>
         <i class="fas fa-plus"></i>
       </span>
+
+      <Modal v-if="showModal" @close="showModal = false">
+        <!--
+      you can use custom content here to overwrite
+      default content
+    -->
+        <h3 slot="header">custom header</h3>
+      </Modal>
   </div>
 </template>
 
 <script>
+import Modal from './common/Modal.vue'
+
 export default {
   data: function() {
     return {
-      newTodoItem: ""
+      newTodoItem: "",
+      showModal: false
     }
   },
   methods: {
@@ -20,12 +31,16 @@ export default {
       if (this.newTodoItem !==''){
         this.$emit('addTodoItem', this.newTodoItem)
         this.clearInput();
+      }else{
+
       }
     },
     clearInput: function() {
       this.newTodoItem='';
     }
-
+  },
+  components: {
+    Modal: Modal
   }
 }
 </script>
--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -18,14 +18,10 @@ export default {
   props:['propsdata'],
   methods: {
     removeTodo: function(todoItem,index){
-    localStorage.removeItem(todoItem);
-    this.todoItems.splice(index, 1);
+      this.$emit('removeItem', todoItem, index);
     },
     toggleComplete: function(todoItem, index){
-      todoItem.completed = !todoItem.completed; 
-      localStorage.removeItem(todoItem.item);
-      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
-      console.log(index);
+      this.$emit('toggleItem', todoItem, index);
     }
   }
 }
